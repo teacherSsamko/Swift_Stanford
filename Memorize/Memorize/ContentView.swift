63c93255038cabe71ae7187faa7b2acebd56bd44
ssamko <teacher.ssamko@gmail.com>
2021-01-03 19:32:56 +0900

    Concept of View

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,10 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack() {
+            ForEach(0..<4) { idx in
+                CardView()
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
     }
 }
+
+struct CardView: View {
+    var isFaceup: Bool = false
+    
+    var body: some View {
+        ZStack() {
+            if isFaceup {
+                RoundedRectangle(cornerRadius: 10.0)
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0)
+                    .stroke(lineWidth: 3.0)
+                    
+                Text("👻")
+                    .font(.largeTitle)
+            } else {
+            RoundedRectangle(cornerRadius: 10.0)
+                .fill()
+            }
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
